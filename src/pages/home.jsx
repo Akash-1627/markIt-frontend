7eefdc66e36eb653893e1fbeeff0adfc248bd4f1
Aneesh <aneeshx000@gmail.com>
2024-05-21 21:00:35 +0530

    data send

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,84 +1,98 @@
-import React, { useRef, useEffect, useState } from 'react';
-import '../style.css';
-import Webcam from 'react-webcam';
-import * as faceapi from 'face-api.js';
-import axios from 'axios';
+import React, { useRef, useEffect, useState } from "react";
+import "../style.css";
+import Webcam from "react-webcam";
+import * as faceapi from "face-api.js";
+import axios from "axios";
 
 const Home = () => {
-    const webcamRef = useRef(null);
-    const [modelsLoaded, setModelsLoaded] = useState(false);
-    // const [sending, setSending] = useState(false);
+  const webcamRef = useRef(null);
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [sending, setSending] = useState(false);
 
-    useEffect(() => {
-        const loadModels = async () => {
-            await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-            await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
-            await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
-            await faceapi.nets.faceExpressionNet.loadFromUri('/models');
-            setModelsLoaded(true);
-        };
-        loadModels();
-    }, []);
+  useEffect(() => {
+    const loadModels = async () => {
+      await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+      await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+      await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+      await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+      setModelsLoaded(true);
+    };
+    loadModels();
+  }, []);
 
-    
-    useEffect(() => {
-        if (!modelsLoaded){
-            console.log("MOdels not loaded");
-            return;
-        };
+  useEffect(() => {
+    if (!modelsLoaded) {
+      console.log("MOdels not loaded");
+      return;
+    }
 
-        const detectFace = async () => {
-            // if (sending) return;
+    const detectFace = async () => {
+      if (sending) return;
 
-            const imageSrc = webcamRef.current.getScreenshot();
-            const img = await faceapi.fetchImage(imageSrc);
-            const detections = await faceapi.detectAllFaces(img, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors().withFaceExpressions();
+      const imageSrc = webcamRef.current.getScreenshot();
+      if (!imageSrc) {
+        console.error("Failed to capture image from webcam");
+        return;
+      }
 
-            if (detections.length > 0) {
-                console.log('Face detected!');
-                // sendImageToBackend(imageSrc);
-            }
-        };
+      try {
+        const img = await faceapi.fetchImage(imageSrc);
+        const detections = await faceapi
+          .detectAllFaces(img, new faceapi.TinyFaceDetectorOptions())
+          .withFaceLandmarks()
+          .withFaceDescriptors()
+          .withFaceExpressions();
 
-        const intervalId = setInterval(detectFace, 3000); // Check for faces every second
+        if (detections.length > 0) {
+            console.log("Detected!!");
+          await sendImageToBackend(imageSrc);
+        }
+      } catch (error) {
+        console.error("Error detecting face:", error);
+      }
+    };
 
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, [modelsLoaded]);
-    
+    const intervalId = setInterval(detectFace, 3000); 
 
-    
+    return () => {
+        clearInterval(intervalId);
+    };
+  }, [modelsLoaded, sending]);
 
-    
-    
-    
-
-    // const sendImageToBackend = async (imageSrc) => {
-    //     try {
-    //         const response = await axios.post('/api/face-detection', { imageSrc });
-    //         console.log('Image sent to backend:', response.data);
-    //         console.log("Image sent");
-    //     } catch (error) {
-    //         console.error('Error sending image to backend:', error);
-    //     } finally {
-    //         setSending(false);
-    //     }
-    // };
-
-    return (
-        <div className='camera-home'>
-            <div className='webcam-container'>
-                <Webcam
-                    audio={false}
-                    ref={webcamRef}
-                    screenshotFormat="image/jpeg"
-                    width={640}
-                    height={480}
-                />
-            </div>
-        </div>
+  const sendImageToBackend = async (imageSrc) => {
+    try {
+    const blob = await fetch(imageSrc).then((res) => res.blob());
+    const formData = new FormData();
+    formData.append("File1", blob, "webcam_image.jpg");
+    const response = await axios.post(
+      "http://localhost:5000/check-face",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
     );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error sending image to backend:", error);
+    } finally {
+      setSending(false);
+    }
+  };
+  return (
+    <div className="camera-home">
+      <div className="webcam-container">
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width={640}
+          height={480}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
